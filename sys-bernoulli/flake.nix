75eac54230f58f604bb8a6f426514666b14a17df
Chloe Bethel <chloe@9net.org>
2022-12-30 19:20:25 +0000

    sys-bernoulli: add device

--- sys-bernoulli/flake.nix
+++ sys-bernoulli/flake.nix
@@ -17,6 +17,7 @@
         ({ inputs, lib, ... }: {
           boot.loader.grub.enable = true;
           boot.loader.grub.version = 2;
+          boot.loader.grub.device = "/dev/sda";
 
           networking.hostName = "bernoulli";
 
