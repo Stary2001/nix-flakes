8b9d7a49856050349317a77230eb093dc0551463
Chloe Bethel <chloe@9net.org>
2022-12-30 19:23:27 +0000

    sys-bernoulli: uk or gb, it is a mystery

--- sys-bernoulli/flake.nix
+++ sys-bernoulli/flake.nix
@@ -25,7 +25,7 @@
           i18n.defaultLocale = "en_GB.UTF-8";
           console = {
             font = "Lat2-Terminus16";
-            keyMap = "gb";
+            keyMap = "uk";
           };
 
           services.openssh.enable = true;
