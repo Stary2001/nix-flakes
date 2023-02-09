6a4cb02f53470c3e8977bb07096eaf5565395a4a
Chloe Bethel <chloe@9net.org>
2023-02-09 08:39:27 +0000

    sys-karman: add flake

---
+++ sys-karman/flake.nix
@@ -0,0 +1,97 @@
+{
+  description = "system configuration for bernoulli";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    common.url = "github:stary2001/nix-flakes?dir=common";
+  };
+
+  outputs = inputs: let
+    system = "x86_64-linux";
+    myKeys = [
+            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJjUz1FruDlg5VNmvd4wi7DiXbMJcN4ujr8KtQ6OhlSc stary@pc"
+            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ+q372oe3sFtBQPAH93L397gYGYrjeGewzoOW97gSy1 stary@wheatley"
+            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOLg5nSbedQYRzm4BAU1OIYpaiTwP+afCAE3BvPcG7OI eddsa-key-20210602" # Windows VM
+          ];
+  in {
+    nixosConfigurations.default = inputs.nixpkgs.lib.nixosSystem {
+      inherit system;
+      specialArgs = { inherit inputs; };
+      modules = [ 
+        (import ./hardware-configuration.nix)
+        (import ./legacy.nix)
+
+        inputs.common.nixosModules.nine-net 
+        inputs.common.nixosModules.ssh-keys 
+
+        ({ inputs, lib, ... }: {
+          nixpkgs.config.allowUnfree = true;
+
+          nix.extraOptions = ''
+            experimental-features = nix-command flakes
+          '';
+
+          boot.loader.systemd-boot.enable = true;
+          boot.loader.efi.canTouchEfiVariables = true;
+          boot.supportedFilesystems = [ "zfs" ];
+
+          networking = {
+            useDHCP = false;
+            wireless.enable = true;
+
+            hostName = "karman"; # Define your hostname.
+            hostId = "3302c071";
+          
+            useNetworkd = true;
+            interfaces = {
+              "enp4s0" = {
+                useDHCP = true;
+              };
+
+              "wlp0s20u2" = {
+                useDHCP = true;
+              };
+            };
+
+            nameservers = [ "8.8.8.8" ];
+          };
+
+          networking.firewall.enable = true;
+          networking.firewall.allowedTCPPorts = [
+            22 # ssh
+            80 # http
+            443 # https
+            5355 # llmnr
+
+            111 2049 4000 4001 4002 20048 # nfsv3
+            5357 # Samba WSDD
+          ];
+
+          networking.firewall.allowedUDPPorts = [
+            111 2049 4000 4001 4002 20048 # nfsv3
+            3702 # Samba WSDD
+          ];
+
+          time.timeZone = "Europe/London";
+          i18n.defaultLocale = "en_GB.UTF-8";
+          console = {
+            font = "Lat2-Terminus16";
+            keyMap = "uk";
+          };
+
+          services.openssh.enable = true;
+
+          users.users.stary = {
+            isNormalUser = true;
+            createHome = true;
+            extraGroups = [ "wheel" ];
+          };
+
+          system.configurationRevision = lib.mkIf (inputs.self ? rev) inputs.self.rev;
+          system.stateVersion = "22.11";
+        })
+      ];
+    };
+  };
+}
---
+++ sys-karman/legacy.nix
@@ -0,0 +1,235 @@
+{config, pkgs, ...}:
+let customSmartdNotify = pkgs.writeScript "smartd-notify.sh" ''
+      #! ${pkgs.runtimeShell}
+      {
+      ${pkgs.coreutils}/bin/cat << EOF
+      From: smartd on ${config.networking.hostName} <automated@9net.org>
+      To: Chloe <chloe@9net.org>
+      Subject: $SMARTD_SUBJECT
+      $SMARTD_FULLMESSAGE
+      EOF
+      ${pkgs.smartmontools}/sbin/smartctl -a -d "$SMARTD_DEVICETYPE" "$SMARTD_DEVICE"
+      } | /run/wrappers/bin/sendmail -i "chloe@9net.org"
+  '';
+in
+{
+  imports = [ ../secrets/wifi.nix ../9net.nix ../avahi.nix ];
+
+  fileSystems."/".options = [ "defaults" "size=2G" "mode=755" ];
+
+  nine_net = {
+    enable = true;
+    node_name = "stary_nas";
+    ipv4_address = "172.31.1.7";
+  };
+
+  boot.initrd.network.ssh = {
+    enable = true;
+    hostKeys = [ "/etc/nixos/config/secrets/ssh_host_rsa_key" "/etc/nixos/config/secrets/ssh_host_ed25519_key" ];
+  };
+
+  boot.extraModulePackages = [
+    # crap usb wifi
+    config.boot.kernelPackages.rtl88x2bu
+  ];
+
+  boot.kernel.sysctl = {
+    # https://docs.syncthing.net/users/faq.html#inotify-limits
+    # Add more inotify watches.
+
+    "fs.inotify.max_user_watches" = 204800;
+  };
+
+  services.smokeping = {
+    enable = true;
+    hostName = "192.168.0.71";
+    host = null;
+
+    targetConfig = ''
+      probe = FPing
+      menu = Top
+      title = Network Latency Grapher
+      remark = Welcome to the SmokePing website of hacking society. \
+               Here you will learn all about the latency of our network.
+
+      + GoogleDNS
+      menu = Google DNS
+      title = Google DNS 8.8.8.8
+      host = 8.8.8.8
+
+      + Local
+      menu = Local
+      title = Router
+      host = 192.168.0.1
+    '';
+  };
+
+  services.syncthing = {
+    enable = true;
+    overrideDevices = false;
+    overrideFolders = false;
+
+    dataDir = "/data/syncthing";
+
+    openDefaultPorts = true;
+  };
+
+  services.nfs.server = {
+    enable = true;
+    exports = ''
+      /export 192.168.0.65(rw,fsid=0,no_subtree_check)
+      /export/syncthing 192.168.0.65(rw,nohide,insecure,no_subtree_check)
+      /export/media 192.168.0.65(rw,nohide,insecure,no_subtree_check)
+    '';
+    lockdPort = 4001;
+    mountdPort = 4002;
+    statdPort = 4000;
+  };
+
+  networking.nat.enable = true;
+  networking.nat.internalInterfaces = ["ve-+"];
+  networking.nat.externalInterface = "enp4s0";
+
+  networking.nat.extraCommands = ''
+    iptables -t nat -A nixos-nat-pre -p tcp -d 172.31.1.7 --dport 3000 -j DNAT --to 10.0.0.2:3000
+  '';
+
+  systemd.network.networks."40-veth" = {
+    name = "ve-*";
+    # Do nothing, hopefully
+  };
+
+  containers.torrent = {
+    autoStart = true;
+
+    privateNetwork = true;
+    hostAddress = "10.0.0.1";
+    localAddress = "10.0.0.2";
+
+    bindMounts = {
+      "/var/lib/rtorrent" = {
+        hostPath = "/var/lib/rtorrent";
+        isReadOnly = false;
+      };
+
+      "/data/rtorrent" = {
+        hostPath = "/data/rtorrent";
+        isReadOnly = false;
+      };
+
+     "/var/lib/flood" = {
+        hostPath = "/var/lib/flood";
+        isReadOnly = false;
+      };
+
+      "/etc/wireguard/mullvad.key" = {
+        hostPath = "/etc/wireguard/mullvad.key";
+        isReadOnly = true;
+      };
+    };
+
+    config = {
+      imports = [ ./container-torrent.nix ];
+    };
+  };
+
+  services.vnstat.enable = true;
+
+  services.zfs.autoSnapshot.enable = true;
+  services.zfs.autoScrub.enable = true;
+
+  users.users.stary.extraGroups = [ "libvirtd" ];
+   virtualisation.libvirtd = {
+    enable = true;
+    onBoot = "ignore";
+    onShutdown = "shutdown";
+
+    #qemu.ovmf.package = pkgs.OVMF.override { secureBoot = true; tpmSupport = true; };
+    #qemu.swtpm.enable = true;
+  };
+
+  services.nginx = {
+    enable = true;
+    recommendedProxySettings = true;
+    virtualHosts = {
+      "karman.9net.org" = { default = true; };
+      "172.31.1.7" = { locations."/" = { root = "/data/misc/web"; extraConfig = "autoindex on;"; }; };
+      "flood.home.9net.org" = { locations."/" = { proxyPass = "http://10.0.0.2:3000"; extraConfig = "allow 172.31.0.3; deny all;"; }; };
+
+      # security: "dude trust me"
+      "syncthing.home.9net.org" = {
+        locations."/" = {
+          extraConfig = ''
+            proxy_set_header Host localhost;
+            proxy_pass http://localhost:8384;
+            allow 172.31.0.3;
+            deny all;
+          '';
+        };
+      };
+
+      "smokeping.home.9net.org" = { locations."/" = { proxyPass = "http://localhost:8081"; extraConfig = "allow 172.31.0.3; deny all;"; }; };
+    };
+  };
+
+  services.plex = {
+    enable = true;
+    openFirewall = true;
+  };
+
+  services.samba-wsdd = {
+    enable = true;
+    interface = "enp4s0";
+    hostname = "karman";
+  };
+
+  services.samba = {
+    enable = true;
+    openFirewall = true;
+    
+    shares = {
+      data = {
+        path = "/data";
+        browseable = "yes";
+        "guest ok" = "no";
+        writeable = "yes";
+        "create mask" = "0644";
+  "directory mask" = "0755";
+        comment = "big data";
+  "valid users" = "stary";
+      };
+    };
+  };
+
+  programs.ssh.forwardX11 = true;
+  services.openssh.forwardX11 = true;
+
+  services.smartd = {
+    enable = true;
+    # Workaround for stock smartctl not including To in its emails...
+    # TODO: PR?
+    notifications.mail.enable = false;
+    notifications.wall.enable = false;
+    notifications.x11.enable = false;
+    defaults.monitored = "-m <nomailer> -M exec ${customSmartdNotify}";
+  };
+
+  programs.msmtp = {
+    enable = true;
+    accounts.default = {
+      auth = true;
+      tls = true;
+      tls_starttls = true;
+      host = "mail9.mymailcheap.com";
+      port = 587;
+
+      user = "automated@9net.org";
+      from = "automated@9net.org";
+
+      passwordeval = "${pkgs.coreutils}/bin/cat /etc/nixos/config/secrets/email-password.txt";
+    };
+  };
+
+  # added on 22.11 upgrade
+  security.polkit.enable = true;  
+}
