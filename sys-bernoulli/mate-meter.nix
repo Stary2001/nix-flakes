b25657fd0faca32ccc7bfd5f4f0c35c7ed67d677
Chloe Bethel <chloe@9net.org>
2023-02-18 14:11:29 +0000

    bernoulli: that's why

--- sys-bernoulli/mate-meter.nix
+++ sys-bernoulli/mate-meter.nix
@@ -11,7 +11,7 @@
 
   systemd.services."mate-meter" = {
     script = ''
-      curl 'https://cdn5.editmysite.com/app/store/api/v18/editor/users/139049822/sites/982743195728728276/products?page=1&per_page=50&sort_by=popularity_score&visibilities\[\]=visible&include=images,category,media_files' \
+      ${pkgs.curl}/bin/curl 'https://cdn5.editmysite.com/app/store/api/v18/editor/users/139049822/sites/982743195728728276/products?page=1&per_page=50&sort_by=popularity_score&visibilities\[\]=visible&include=images,category,media_files' \
   -H 'authority: cdn5.editmysite.com' \
   -H 'accept: application/json, text/plain, */*' \
   -H 'accept-language: en-GB,en-US;q=0.9,en;q=0.8' \
@@ -26,7 +26,7 @@
   -H 'sec-fetch-mode: cors' \
   -H 'sec-fetch-site: cross-site' \
   -H 'user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.0.0 Safari/537.36' \
-  --compressed | ${pkgs.jq}/bin/jq -r '.data | .[] | "\(.name) \(.inventory.total)"' | sed 's/$/<br>/g' > /home/stary/www/mate-meter/index.html
+  --compressed | ${pkgs.jq}/bin/jq -r '.data | .[] | "\(.name) \(.inventory.total)"' | ${pkgs.coreutils}/bin/sed 's/$/<br>/g' > /home/stary/www/mate-meter/index.html
     '';
 
     serviceConfig = {
